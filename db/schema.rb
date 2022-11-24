c668a36b1b47cd9a3f637fcd25289741ba0bf23f
Alice Heber <alice.heber@gmail.com>
2022-11-24 12:52:10 +0000

    fixed after merge

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_23_123943) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_24_123655) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -56,7 +56,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_23_123943) do
   create_table "pitches", force: :cascade do |t|
     t.string "name"
     t.string "sport"
-    t.string "address"
+    t.text "address"
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
