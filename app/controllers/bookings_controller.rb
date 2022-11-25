2a1f5734d9a475141761b6c30d6b138b519f9191
Philippe Paoli <philippe.paoli11@gmail.com>
2022-11-25 14:52:24 +0000

    listings of pitches + booking process + booking landing page

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,24 +1,44 @@
 class BookingsController < ApplicationController
-  before_action :set_pitch, only: %i[new create]
+  before_action :set_pitch, only: %i[new create index]
 
   def new
     @booking = Booking.new
   end
 
+  def index
+    @bookings = Booking.where(user_id: current_user.id)
+  end
+
+  def confirmation
+    set_booking
+  end
+
+
   def create
     @booking = Booking.new(booking_params)
     @booking.pitch = @pitch
-    if @booking.save
-      redirect_to pitch_path(@pitch)
+    @booking.user = current_user
+    if @booking.save!
+      redirect_to booking_confirmation_path(@booking)
     else
-      render :new, status: :unprocessable_entity
+      redirect_to pitch_path(@path), status: :unprocessable_entity
     end
+  end
+
+  def show
+    set_booking
+    @booking.save
+    redirect_to booking_path(@booking)
   end
 
   private
 
   def set_pitch
     @pitch = Pitch.find(params[:pitch_id])
+  end
+
+  def set_booking
+    @booking = Booking.find(params[:id])
   end
 
   def booking_params
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
   resources :pitches do
-    resources :bookings, only: [:new, :create]
+    resources :bookings, only: [:new, :create, :index]
   end
+  get 'bookings/:id/confirmation', to: "bookings#confirmation", as: :booking_confirmation
 end
